eb9b420768c48efb27da84f8330a5001db320719
tsuarai-1977 <tsuarai-1977@users.noreply.github.com>
2026-03-09 22:38:36 +0900

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/anticipatory-burden-tracker/sw.js', { scope: '/anticipatory-burden-tracker/' })})}
